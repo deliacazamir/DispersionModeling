611176ac96fcedee91a41794426861db344f6347
deliacazamir <delia.cazamir@gmail.com>
2019-06-11 12:05:20 +0300

    Power Plant Drop Down

--- DispersionModeling-SPA/src/app/Forms/user-data-form/user-data-form.component.ts
+++ DispersionModeling-SPA/src/app/Forms/user-data-form/user-data-form.component.ts
@@ -1,18 +1,26 @@
 import { StationTypeService } from './../../_services/station-type.service';
 import { Component, OnInit } from '@angular/core';
-// import {MatSelectModule} from '@angular/material/select';
+
 
 @Component({
   selector: 'app-user-data-form',
   templateUrl: './user-data-form.component.html',
   styleUrls: ['./user-data-form.component.scss']
 })
+
 export class UserDataFormComponent implements OnInit {
 
   constructor(public service: StationTypeService) { }
 
+  id:number;
+  
   ngOnInit() {
+
     this.service.refreshList();
+  }
+
+  selected(){
+    console.log(this.id)
   }
 
 }
--- DispersionModeling-SPA/src/app/app.module.ts
+++ DispersionModeling-SPA/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { AgmCoreModule } from '@agm/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
- 
 import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -19,6 +19,8 @@ import { PollutantListService } from './_services/pollutant-list.service';
 import { PollutionSourceFormComponent } from './Forms/pollution-source-form/pollution-source-form.component';
 import { DispersionModelFormComponent } from './Forms/dispersion-model-form/dispersion-model-form.component';
 import { UserDataFormComponent } from './Forms/user-data-form/user-data-form.component';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
+
 
 @NgModule({
    declarations: [
@@ -37,8 +39,9 @@ import { UserDataFormComponent } from './Forms/user-data-form/user-data-form.com
       AgmCoreModule.forRoot({apiKey: 'Insert key here'}),
       MDBBootstrapModule.forRoot(),
       BrowserAnimationsModule,
-      ToastrModule.forRoot()
+      ToastrModule.forRoot(),
    ],
+ 
    providers: [
       AuthService,
       CreateMapService,
@@ -48,5 +51,6 @@ import { UserDataFormComponent } from './Forms/user-data-form/user-data-form.com
    bootstrap: [
       AppComponent
    ]
+   
 })
 export class AppModule { }
